50348f69aeb11ef181e6d472a62c84b7b701254d
nomuvan <nomuvan@gmail.com>
2014-05-08 07:55:48 +0900

    modify

--- launchsupport.gemspec
+++ launchsupport.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Launchsupport::VERSION
   spec.authors       = ["nomuvan"]
   spec.email         = ["nomuvan@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Launch Support}
+  spec.description   = %q{Launch Support}
   spec.homepage      = ""
   spec.license       = "MIT"
 
--- lib/launchsupport.rb
+++ lib/launchsupport.rb
@@ -1,5 +1,31 @@
 require "launchsupport/version"
 
-module Launchsupport
-  # Your code goes here...
+module Kernel
+
+  def on_launching file, param={}, &b
+    LaunchSupport::launching_my_file_process(file, param, &b) if $0 == file
+  end
+
+  def not_on_launching file, param={}, &b
+    LaunchSupport::launching_my_file_process(file, param, &b) unless $0 == file
+  end
+
+end
+
+module LaunchSupport
+
+  def self.launching_my_file_process file, param={}
+    if param
+      paths = param[:load]
+      paths = [paths] if paths.is_a? String
+      param[:project] ||= file.split("/")[file.split("/").rindex{|x|paths.include?(x)}-1]
+      parts = "/#{param[:project]}/"
+      paths.each do |path|
+        $:.unshift file[0,file.index(parts)+parts.size-1]+"/#{path}"
+      end
+      [param[:require]].flatten.each{|r|require r} if param[:require]
+    end
+    yield if block_given?
+  end
+
 end
